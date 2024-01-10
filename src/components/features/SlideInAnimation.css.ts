b65d5c3ae4dc8ed4ef3ccc77083a791929d6a271
sivamw6 <105441569+sivamw6@users.noreply.github.com>
2024-01-10 23:49:33 +1100

    Update SlideInAnimation.css.ts with opacity values as strings

--- src/components/features/SlideInAnimation.css.ts
+++ src/components/features/SlideInAnimation.css.ts
@@ -3,12 +3,12 @@ import { style } from '@vanilla-extract/css';
 
 
 export const slideIn = style({
-  opacity: 0,
+  opacity: '0',
   transform: 'translateY(40%)',
   transition: 'all 1s ease-out',
 });
 
 export const isVisible = style({
-  opacity: 1,
+  opacity: '1',
   transform: 'translateY(0%)',
 });
